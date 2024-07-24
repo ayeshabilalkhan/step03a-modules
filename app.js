e492219ec7b726d8fa4d3108fa342ea364971a20
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-24 22:32:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var first_js_1 = require("./first.js");
+console.log((0, first_js_1.default)(7, 4));
---
+++ app.ts
@@ -0,0 +1,2 @@
+import add from "./first.js";
+console.log(add(7,4));
